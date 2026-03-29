9ca93097f38e8a1990fe6bb111f643f8a2650328
Camilo Morales <114544629+Camilo959@users.noreply.github.com>
2026-03-29 11:11:51 -0500

    fix: remove duplicated 'the' in Abstract Factory TypeScript example

--- src/AbstractFactory/Conceptual/index.ts
+++ src/AbstractFactory/Conceptual/index.ts
@@ -95,7 +95,7 @@ class ConcreteProductA2 implements AbstractProductA {
 }
 
 /**
- * EN: Here's the the base interface of another product. All products can
+ * EN: Here's the base interface of another product. All products can
  * interact with each other, but proper interaction is possible only between
  * products of the same concrete variant.
  *
